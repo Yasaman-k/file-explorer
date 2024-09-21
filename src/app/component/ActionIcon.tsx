853d3146ad75c73d5b3597fe533af4930f51f4a5
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-09-21 14:06:11 +0330

    default page

--- src/app/component/ActionIcon.tsx
+++ src/app/component/ActionIcon.tsx
@@ -4,9 +4,18 @@ import Image from 'next/image'
 
 const ActionIcon = () => {
     return (
-        <>
-            <Image src={`add-file.svg`} alt={'add-file'} width="10" />
-        </>
+        <div className='img-container'>
+            <button onClick={() => {
+                console.log("hi9");
+            }} >
+                <Image src={`add-file.svg`} alt={'add-file'} width="30" height="30" />
+            </button>
+            <button><Image src={`add-folder.svg`} alt={'add-folder'} width="30" height="30" /></button>
+            <button>
+                <Image src={`delete.svg`} alt={'deleted'} width="30" height="30" />
+            </button>
+
+        </div >
     )
 }
 export default ActionIcon;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import ActionIcon from "./component/actionIcon";
+import ActionIcon from "./component/ActionIcon";
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div style={{ padding: '4rem' }}>
       <main>
-        <div style={{ display: "flex", alignItems: 'center' }}>
-          <div className="box-style">Root</div>
+        <div style={{ display: "flex", alignItems: 'center', gap: '20px' }}>
+
+
+          <div className="box-style" style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+            <Image src={`folder.svg`} alt={'add-file'} width="15" height="15" />
+            <span> Root</span>
+          </div>
+
           <ActionIcon />
         </div>
       </main >
